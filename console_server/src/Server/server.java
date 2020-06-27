ca43d68167d3afaab377aa4246f5587d86b1fcb9
patrickmcdermott <patrickmcdermott@patricks-mbp.fios-router.home>
2020-06-27 17:57:32 -0400

    server class

---
+++ console_server/src/Server/server.java
@@ -0,0 +1,18 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+	
+	 private ServerSocket server;  // server socket
+	 private Socket connection;    // connection to client
+	 
+	 public void runServer()
+	 {
+	 
+	 }
+
+	
+}
